3d02b54e507e8a30b2b4358221139bd9243b822b
vishal Kshyatriya <136780599+vishalkhyatriya12@users.noreply.github.com>
2023-08-08 07:28:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi All, I'm Vishal.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View my React Source code here on my Github Profile.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
